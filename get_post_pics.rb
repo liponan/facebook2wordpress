753a3998c59cfa970f953d4d321660601b6ce03b
Po-Nan Li <lishantreasure@gmail.com>
2015-06-02 01:40:44 +0800

    can handle no creator exception

--- get_post_pics.rb
+++ get_post_pics.rb
@@ -35,8 +35,8 @@ count = 0;
 data_hash["data"].each do |datum|
 	
 	title = datum["message"].scan(pat).to_a
-	puts title[0]
-	puts datum["message"]
+	# puts title[0]
+	# puts datum["message"]
 	
 	post_hash = {"item"=>{}}
 	post_hash = {}
@@ -56,6 +56,9 @@ data_hash["data"].each do |datum|
 		count = count + 1
 		post_hash["title"] = "未命名-" + count.to_s
 	end
+
+	puts post_hash["title"]
+
 	# post_hash["item"]["content:encoded"] = "<![CDATA[" + datum["message"] + "]]>"
 	post_hash["content:encoded"] = "<![CDATA[" + datum["message"] + "]]>"
 	# post_hash["item"]["pubDate"] = datum["created_time"]
@@ -67,9 +70,13 @@ data_hash["data"].each do |datum|
 		datum["created_time"][17..18])
 	post_hash["pubDate"] = created_time.strftime('%F %T')
 	# post_hash["item"]["dc:creater"] = datum["admin_creator"]["name"]
-	post_hash["dc_creator"] = datum["admin_creator"]["name"]
+	if datum["admin_creator"].nil?
+		post_hash["dc_creator"] = "每日一冷"
+	else
+		post_hash["dc_creator"] = datum["admin_creator"]["name"]
+	end
 	post_hash["wp:post_type"] = "post"
-
+	post_hash["wp:status"] = "publish"
 	obj_url = "https://graph.facebook.com/" + datum["id"] + "/?fields=full_picture"
 	obj_url = obj_url + '&access_token=' + token
 	json = open(obj_url)
@@ -87,10 +94,10 @@ data_hash["data"].each do |datum|
 	xml_hash["channel"] << post_hash
 
 
-	puts post_hash
+	# puts post_hash
 end
 
-puts xml_hash
+# puts xml_hash
 xml = xml_hash.to_xml
 # xml = xml_hash.to_json.to_xml
 # puts xml
@@ -98,7 +105,7 @@ open("dailycold_archieve.xml", 'wb') do |file|
 	open("xml_template.txt", 'r') do |tmp|
 		file << tmp.read
 	end
-	file << xml
+	file << xml[18..(xml.length-1)]
 	file << "</rss>"
 end
 
